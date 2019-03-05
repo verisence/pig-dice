7ed7206b4aec57400d17a25b3ca6e7589cfe033c
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-03-05 12:50:57 +0300

    Add total score and final score

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ function Gamer(name){
   // this.score = 0;
   // this.finalScore = 0;
 }
+function reset1(){
 
+}
 var score1 = 0;
 var score2 = 0;
 var finalScore1 = 0;
@@ -31,8 +33,20 @@ $(document).ready(function(){
       $("#player1Roll").text(rollOne);
       $("#player1Score").text(score1);
     });
+
+    $("#roll2").click(function(){
+      var rollTwo = rollDice();
+      score2 += rollTwo;
+      $("#player2Roll").text(rollTwo);
+      $("#player2Score").text(score2);
+    });
   });
-  $("hold1").click(function(){
+  $("#hold1").click(function(){
     finalScore1 += score1;
+    $("#player1Final").text(finalScore1);
+  })
+  $("#hold2").click(function(){
+    finalScore2 += score2;
+    $("#player2Final").text(finalScore2);
   })
 });
