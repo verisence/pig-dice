3eaf984c2689700b8559985b5bed518993ae5d37
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-03-05 12:42:45 +0300

    Add gamer objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 function Gamer(name){
   this.name = name;
-  this.score = 0;
-  this.finalScore = 0;
+  // this.score = 0;
+  // this.finalScore = 0;
 }
+
+var score1 = 0;
+var score2 = 0;
+var finalScore1 = 0;
+var finalScore2 = 0;
 
 function rollDice(){
   var diceNumber = Math.floor(Math.random()*6)+1;
@@ -11,17 +16,23 @@ function rollDice(){
 $(document).ready(function(){
   $("#start").click(function(event){
     event.preventDefault();
-    var playerOne = $("playerOne").val();
-    var playerTwo = $("playerTwo").val();
+    var playerOne = $("#player1").val();
+    var playerTwo = $("#player2").val();
     var gamerOne = new Gamer(playerOne);
     var gamerTwo = new Gamer(playerTwo);
+    $("#player1Name").text(gamerOne.name);
+    $("#player2Name").text(gamerTwo.name);
     $("#welcome").hide();
     $("#game").show();
 
-    $("#player1Roll").click(function(){
-      
+    $("#roll1").click(function(){
+      var rollOne = rollDice();
+      score1 += rollOne;
+      $("#player1Roll").text(rollOne);
+      $("#player1Score").text(score1);
     });
   });
-
-
+  $("hold1").click(function(){
+    finalScore1 += score1;
+  })
 });
