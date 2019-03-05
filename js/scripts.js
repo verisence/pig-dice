49a7aad3a5ba01f2ee9c185d60a941d2b5400ece
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-03-05 11:22:54 +0300

    Add function to generate dice rolls

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,15 @@ function Gamer(name){
   this.finalScore = 0;
 }
 
+function rollDice(){
+  var diceNumber = Math.floor(Math.random()*6)+1;
+  return diceNumber;
+}
 $(document).ready(function(){
   $("#start").click(function(event){
     event.preventDefault();
     $("#welcome").hide();
   });
 
-    
+var playerOne = $("")
 });
